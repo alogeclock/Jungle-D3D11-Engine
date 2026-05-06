6c42137f609cf5a4075185b23bb7066dd8617f50
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-05-06 13:52:40 +0900

    Feat: Add UCurveFloat Implementation

--- Week10/KraftonEngine/Source/Engine/Math/CurveFloat.h
+++ Week10/KraftonEngine/Source/Engine/Math/CurveFloat.h
@@ -8,5 +8,8 @@ public:
 	TArray<FVector2> Curve;
 
 public:
-	FVector2 Evaluate(float NormalizedT) const;
+	float Evaluate(float NormalizedT) const;
+	void AddKey(float Time, float Value);
+	void SortKeys();
+	void ResetLinear();
 };
