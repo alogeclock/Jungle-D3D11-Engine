75ddb7aee7c8591acbfe7a1c632d7a3b13e9fa93
alogeclock <alogeclock@gmail.com>
2026-05-13 01:54:54 +0900

    Hotfix: 특정 파일 누락 수정

---
+++ Week10/LunaticEngine/Source/Editor/UI/EditorCommonWidgetUtils.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "Core/CoreTypes.h"
+
+#include <functional>
+
+namespace FEditorCommonWidgetUtils
+{
+	bool DrawLabeledField(const char* Label, const std::function<bool()>& DrawField);
+	void DrawReadOnlyField(const char* Label, const FString& Value);
+
+	bool DrawColoredFloat2(const char* Label, float Values[3], float Speed, bool bShowReset = false, const float* ResetValues = nullptr);
+	bool DrawColoredFloat3(const char* Label, float Values[3], float Speed, bool bShowReset = false, const float* ResetValues = nullptr);
+	bool DrawColoredFloat4(const char* Label, float Values[4], float Speed, bool bShowReset = false, const float* ResetValues = nullptr);
+	bool DrawNamedFloat4(const char* Label, float Values[4], float Speed, const char* AxisLabels[4], bool bShowReset = false, const float* ResetValues = nullptr);
+}
