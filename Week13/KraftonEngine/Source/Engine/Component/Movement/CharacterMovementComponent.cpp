fe234b7317ef2e73e01df278f99ae5e0df32c1da
keonwookang0914 <kwkang0914@gmail.com>
2026-05-19 17:11:58 +0900

    feat: CharacterMovementComponent 뼈대 작성
    수정한 파일:CharacterMovementComponent.h
    CharacterMovementComponent.cpp
    
    추가한 내용:UCharacterMovementComponent : public UMovementComponent 껍데기 정리
    UCLASS(), GENERATED_BODY(UCharacterMovementComponent) 최신 리플렉션 매크로 유지
    BeginPlay, TickComponent, Serialize, PostEditProperty에서 UMovementComponent Super 호출
    UPrimitiveComponent* UpdatedPrimitive 런타임 캐시 멤버 추가
    
    수정한 이유:1단계 목표대로 이동/입력/UPROPERTY 로직 없이 빌드와 리플렉션 등록만 확인하기 위해 정리했습니다.
    
    빌드 결과:성공
    MSBuild .\KraftonEngine.sln /p:Configuration=Debug /p:Platform=x64 /nologo /v:minimal

---
+++ Week13/KraftonEngine/Source/Engine/Component/Movement/CharacterMovementComponent.cpp
@@ -0,0 +1,26 @@
+﻿#include "CharacterMovementComponent.h"
+#include "Component/PrimitiveComponent.h"
+#include "Serialization/Archive.h"
+
+void UCharacterMovementComponent::BeginPlay()
+{
+	UMovementComponent::BeginPlay();
+	UpdatedPrimitive = Cast<UPrimitiveComponent>(GetUpdatedComponent());
+}
+
+void UCharacterMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction& ThisTickFunction)
+{
+	UMovementComponent::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	UpdatedPrimitive = Cast<UPrimitiveComponent>(GetUpdatedComponent());
+}
+
+void UCharacterMovementComponent::Serialize(FArchive& Ar)
+{
+	UMovementComponent::Serialize(Ar);
+}
+
+void UCharacterMovementComponent::PostEditProperty(const char* PropertyName)
+{
+	UMovementComponent::PostEditProperty(PropertyName);
+	UpdatedPrimitive = Cast<UPrimitiveComponent>(GetUpdatedComponent());
+}
---
+++ Week13/KraftonEngine/Source/Engine/Component/Movement/CharacterMovementComponent.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "Core/EngineTypes.h"
+#include "MovementComponent.h"
+#include "CharacterMovementComponent.generated.h"
+
+class UPrimitiveComponent;
+
+/*
+ * ACharacter가 사용할 경량 CharacterMovement API의 기반 컴포넌트입니다.
+ * 실제 이동/입력/회전 로직은 단계별로 추가합니다.
+ */
+UCLASS()
+class UCharacterMovementComponent : public UMovementComponent
+{
+public:
+	GENERATED_BODY(UCharacterMovementComponent)
+
+	UCharacterMovementComponent() = default;
+	~UCharacterMovementComponent() override = default;
+
+	void BeginPlay() override;
+	void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction& ThisTickFunction) override;
+	void Serialize(FArchive& Ar) override;
+	void PostEditProperty(const char* PropertyName) override;
+
+private:
+	UPrimitiveComponent* UpdatedPrimitive = nullptr;
+};
+
